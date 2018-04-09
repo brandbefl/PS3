88fa99160dbd28af989451b7b76d372379511d36
brandbefl <artgranat@list.ru>
2018-04-09 18:14:14 +0300

    hamburger menu css and js

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
         nav: false,
         dots: true
     });
+    $(".menu-trigger").click(function(){
+        $(".ul-menu").slideToggle(500);
+    });
+    $(window).resize(function() {
+        if ($(window).width() > 500 ) {
+            $(".ul-menu").removeAttr("style");
+        }
+    });
 });
